45141d72178b78c31459be6185ba088395b63f81
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-08-02 11:28:49 +0530

    Create current_samples.c

---
+++ current_samples.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "current_samples.h"
+
+rangeAndReadings inRangeReading(int* currentInAmpere, int sizeOfcurrentInAmpere) {
+  rangeAndReadings rangeReadings;
+  rangeReadings.numberOfReadingsInRange = 1;
+  std::sort(currentInAmpere, currentInAmpere+sizeOfcurrentInAmpere);
+  for(int counter = 0; counter <sizeOfcurrentInAmpere; counter++) {
+     if (((currentInAmpere[counter] + 1) == (currentInAmpere[counter +1])) || (currentInAmpere[counter] == currentInAmpere[counter+1])){
+        rangeReadings.numberOfReadingsInRange++;
+     }
+  }
+  rangeReadings.range[0] = currentInAmpere[0];
+  rangeReadings.range[1] = currentInAmpere[sizeOfcurrentInAmpere -1];
+  return rangeReadings;
+}
