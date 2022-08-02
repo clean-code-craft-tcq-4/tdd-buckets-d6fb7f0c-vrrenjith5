9b68a3d66f739ebd0aabdaf96f9df29af7b84c3d
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-08-02 11:29:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
+#include "current_samples.h"
 
 TEST_CASE("test current set 1") {
 int currentInAmpere[]  = {4,5,6};
